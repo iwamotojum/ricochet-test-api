a6b120b0025fe85fb5e11bfc6d9bb191e6d4efb5
Jum Iwamoto <iwamoto.jum@gmail.com>
2025-01-08 02:03:48 -0300

    adding migration to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
-# Start PHP-FPM
+# Rodar as migrations
+echo "Rodando as migrations..."
+php artisan migrate --force
+
+# Gerar a chave do Laravel (caso não tenha sido gerada ainda)
+echo "Gerando a chave do Laravel..."
+php artisan key:generate --force
+
+# Iniciar o PHP-FPM
+echo "Iniciando o PHP-FPM..."
 php-fpm -D
 
-# Start Nginx
+# Iniciar o Nginx
+echo "Iniciando o Nginx..."
 nginx -g "daemon off;"
